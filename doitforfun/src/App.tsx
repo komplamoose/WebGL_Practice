c4b0b7a0319541ff12719f4f85ddf6981b589d0b
komplamoose2 <iamjewel4445@gmail.com>
2023-01-27 00:04:20 +0900

    feat: GLSL 추가

--- doitforfun/src/App.tsx
+++ doitforfun/src/App.tsx
@@ -1,6 +1,22 @@
 import React, {useEffect, useRef} from "react";
 import styled from "styled-components";
 
+const VERTEX_SHADER = `
+  attribute vec4 a_position;
+  
+  void main() {
+    gl_position = a_position;
+  }
+`;
+
+const FRAGMENT_SHADER = `
+  precision mediump float;
+  
+  void main() {
+    gl_FragColor = vec4(1, 0, 0.5, 1);
+  }
+`;
+
 const App : React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -24,6 +40,18 @@ const App : React.FC = () => {
     const gl = canvas.getContext("webgl");
     if (!gl) alert("WebGL을 지원하지 않는 브라우저입니다.");
     return gl;
+  }
+
+  const createShader = (gl : WebGLRenderingContext, type : number, source : string) => {
+    const shader = gl.createShader(type);
+    if (!shader) return;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    const isSuccess = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (isSuccess) return shader;
+
+    gl.deleteShader(shader);
   }
 
 
